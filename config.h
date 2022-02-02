7180f80f6dfd6d327504e15734588e6127a279b2
Akshansh Bhanjana <akshansh2000@gmail.com>
2022-02-02 15:26:17 +0530

    workspace for lyrics

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "L", "S" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Ferdi",    NULL,       NULL,       1 << 9,       0,           -1 },
 };
 
 /* layout(s) */
@@ -125,6 +125,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_minus,                  9)
+	TAGKEYS(                        XK_equal,                  10)
 	// { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
